958742587222792eb19eb9ff6e4428d5b23072d8
wissal <wissal.ouarsafi@gmail.com>
2020-01-28 19:13:07 -0800

    create userPlaces route and page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 import Users from './users/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Users />
+          </Route>
+          <Route exact path="/:userId/places">
+            <UserPlaces />
           </Route>
           <Route exact path="/places/new">
             <NewPlace />
