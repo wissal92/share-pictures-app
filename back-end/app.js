cda4dc9210b3e7a07d45cf9ac9138f03120c473d
wissal <wissal.ouarsafi@gmail.com>
2020-02-02 17:41:45 -0800

    Install mongoose and connect my back-end with mongodb

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
 const HttpError = require('./models/http-error')
@@ -25,4 +26,8 @@ app.use((error, req, res, next) => {
     res.status(error.code || 500).json({message: error.message || 'An unknown error occurred!'});
 });
 
-app.listen(5000);
+mongoose
+      .connect('mongodb+srv://wissal14:B4Q9BLSFpfbD709D@cluster0-tfoyl.mongodb.net/places?retryWrites=true&w=majority')
+      .then(() => app.listen(5000))
+      .catch(err => console.log(err))
+
